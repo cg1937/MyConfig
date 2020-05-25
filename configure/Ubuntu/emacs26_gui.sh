a97785d198acfb98c27a600f9a86af116412933a
cg1937 <522588429@qq.com>
2020-05-25 22:49:35 +0800

    fix problem

---
+++ configure/Ubuntu/emacs26_gui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# successful on Ubuntu 18.04 WSL
+echo "安装相关依赖"
+sudo apt-get install -y dbus-x11
+sudo apt-get install -y xfce4
+sudo apt-get install -y xorg-dev 
+sudo apt-get install -y build-essential libncurses5-dev
+sudo apt-get install -y libgif-dev libxpm-dev
+sudo apt-get install -y libtiff5-dev
+sudo apt-get install -y libxml2-dev
+sudo apt-get install -y libgnutls28-dev
+echo "在profile文件添加相关配置选项"
+echo -e "export DISPLAY=:0.0\nexport NO_AT_BRIDGE=1" | sudo tee -a /etc/profile
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt update
+sudo apt-get install -y emacs26
--- configure/Ubuntu/update_mirror_18.04.sh
+++ configure/Ubuntu/update_mirror_18.04.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# successfull on ubuntu18.04 wsl
+# successful on Ubuntu 18.04 WSL
 echo "备份原有的镜像源配置"
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 echo "配置清华源..."
-echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+sudo cat>>~/sources.list<<EOF
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
@@ -12,14 +13,20 @@ deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricte
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-
 # 预发布软件源，不建议启用
 # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
-" | sudo tee /etc/apt/sources.list
-sudo apt-get update && sudo apt-get upgrade
+EOF
+sudo mv ~/sources.list /etc/apt/
+sudo apt-get update && sudo apt-get -y upgrade
 echo "更新完成"
-echo "添加windows字体"
+echo "添加windows字体, 刷新字体缓存"
 sudo mkdir /usr/share/fonts/windows
 sudo cp -r /mnt/c/Windows/Fonts/*.ttf /usr/share/fonts/windows/
+sudo apt-get install -y fontconfig
 fc-cache
+echo "修正raw.githubusercontent.com DNS污染问题"
+echo "199.232.68.133 raw.githubusercontent.com" | tee -a /etc/hosts
+echo "安装screenfetch并打印系统信息"
+sudo apt-get install -y screenfetch
+screenfetch
