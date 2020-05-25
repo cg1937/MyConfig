d568c6d5c81618a1cd2a60886248f5055560a295
cg1937 <522588429@qq.com>
2020-05-25 10:19:46 +0800

    update the wsl ubuntu18.04 configure

---
+++ configure/Common/scripts/update_mirror_ubuntu18.04.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# successfull on ubuntu18.04 wsl
+echo "备份原有的镜像源配置"
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "配置清华源..."
+echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+" | sudo tee /etc/apt/sources.list
+sudo apt-get update && sudo apt-get upgrade
+echo "更新完成"
+echo "添加windows字体"
+sudo mkdir /usr/share/fonts/windows
+sudo cp -r /mnt/c/Windows/Fonts/*.ttf /usr/share/fonts/windows/
+fc-cache
