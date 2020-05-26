6fd30ac0cdbe86d9d3553adcada79f780193a18d
cg1937 <522588429@qq.com>
2020-05-26 11:58:39 +0800

    add git srcipt and test org-mode file

---
+++ configure/Common/scripts/add_git_SSH_key.sh
@@ -0,0 +1,7 @@
+git config --global  user.name "cg1937"
+git config --global user.email "522588429@qq.com"
+ssh-keygen -t rsa -C "522588429@qq.com"
+cat ~/.ssh/id_rsa.pub | xsel --clipboard
+# xsel 用于将内容发送到剪切板
+echo "the context of id_rsa.pub is send to your clipboard, enjoy it!"
+echo "remember use 'ssh -T git@github.com' to test"
