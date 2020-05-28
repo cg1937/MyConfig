bd922fc00fdb8bc5b6bf167634a448c9d2a92279
cg1937 <522588429@qq.com>
2020-05-28 20:18:52 +0800

    add font install in script

--- configure/Ubuntu/emacs26_gui.sh
+++ configure/Ubuntu/emacs26_gui.sh
@@ -10,6 +10,8 @@ sudo apt-get install -y libtiff5-dev
 sudo apt-get install -y libxml2-dev
 sudo apt-get install -y libgnutls28-dev
 echo "在profile文件添加相关配置选项"
+echo "安装文泉驿-微米黑, 用于设置emacs-cnfonts"
+sudo apt-get install -y ttf-wqy-microhei
 echo -e "export DISPLAY=:0.0\nexport NO_AT_BRIDGE=1" | sudo tee -a /etc/profile
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt update
